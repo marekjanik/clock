9eb28cb208b4e10fabf2d4280eccea609ba85928
Marek Janik <marekadamjanik@gmail.com>
2021-05-18 14:35:50 +0200

    add hours functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const secondHand = document.querySelector(".second-hand");
 const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date();
@@ -11,6 +12,10 @@ function setDate() {
   const minutes = now.getMinutes();
   const minutesDegrees = (minutes / 60) * 360 + 90;
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 60) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
